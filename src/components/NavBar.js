42e114d4dd23e2bcda385dac3da293865cdec5f1
dataseek <dataseek@gmail.com>
2022-01-19 19:39:30 -0300

    fecha

---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+import"./NavBar.css";
+
+const NavBAr = ()=>{
+
+
+return <nav>
+    <ul>
+        <li>Home </li>
+        <li>Galeria </li>
+        <li>Info</li>
+        <li>Contacto</li>
+    </ul>
+</nav>
+};
+export default NavBAr;
