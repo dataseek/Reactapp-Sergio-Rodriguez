e40324338921565fcb9115d529350152080bc6f8
Sergio Rodriguez <dataseek@gmail.com>
2022-01-23 15:19:23 -0300

    Ecommmerce-RodriguezSergio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar/NavBar';
+import React, { Component } from 'react';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit Holas prueba definitiva<code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar
+					item1='Home'
+					item2='Servicios'
+          item3='Nosotros'
+					item4='Contacto'
+				/>
     </div>
   );
 }
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import './NavBar.css';
+import img from './logo192.png';
+import Cart from '../CartWidget/CartWidget';
+
+const NavBar = (props) => {
+	return (
+<>
+<nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <a className="navbar-brand" href="#"><img src={img} width={60} alt='logo-coder-house' /></a>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+  <div className="collapse navbar-collapse d-flex justify-content-center" id='navbarNav'>
+    <ul className="navbar-nav">
+      <li className='nav-item active'>
+        <a className='nav-link' href='#'>{props.item1}<span className='sr-only'></span></a>
+      </li>
+      <li className='nav-item'>
+        <a className='nav-link' href='#'>{props.item2}</a>
+      </li>
+      <li className='nav-item'>
+        <a className='nav-link' href='#'>{props.item3}</a>
+      </li>
+      <li className='nav-item'>
+        <a className='nav-link' href='#'>{props.item4}</a>
+      </li>
+      <li className='nav-item'>
+        <Cart
+        color='blue'
+        caption=' 4 '
+        />
+      </li>
+    </ul>
+  </div>
+</nav>
+</>
+	);
+};
+
+export default NavBar;
